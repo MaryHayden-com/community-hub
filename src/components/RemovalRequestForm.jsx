9ebcce68677d9f6f8d01b432ea33e41b8554ee10
Mary Hayden <mary@maryhayden.com>
2026-04-16 22:27:47 +0000

    File changes

--- src/components/RemovalRequestForm.jsx
+++ src/components/RemovalRequestForm.jsx
@@ -22,7 +22,7 @@ export default function RemovalRequestForm({ listing, onClose }) {
     }
     setSaving(true);
     await base44.integrations.Core.SendEmail({
-      to: "privacy@communityhub.ie",
+      to: "mary@maryhayden.com",
       subject: `Listing Removal Request: ${listing.name}`,
       body: `Listing Removal Request\n\nListing: ${listing.name} (ID: ${listing.id})\nCounty: ${listing.county}\n\nRequested by: ${form.name}\nEmail: ${form.email}\n\nReason:\n${form.reason}`,
     });
